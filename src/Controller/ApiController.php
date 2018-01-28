f43c8d6053d988546b1fb1f68463826d5930d02e
Andrii Navrotskii <andrii.navrotskii@gmail.com>
2018-01-28 02:25:58 +0200

    serializer

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -14,6 +14,10 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class ApiController extends Controller
 {
@@ -45,6 +49,7 @@ class ApiController extends Controller
         $encoders = array(new XmlEncoder(), new JsonEncoder());
         $normalizers = array(new ObjectNormalizer());
         $serializer = new Serializer($normalizers, $encoders);
+
         return new JsonResponse($serializer->serialize($data, 'json'), $code, [], true);
     }
 }
